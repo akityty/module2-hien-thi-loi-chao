e1917ad3898f1fbb9dd4d887f5ee484310af7676
akityty <congnv.9803@gmail.com>
2019-07-23 16:23:28 +0700

    ok

---
+++ src/sayHello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class sayHello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap ten cua ban");
+        String name = scanner.nextLine();
+        System.out.println("Xin Chao "+name);
+    }
+}
